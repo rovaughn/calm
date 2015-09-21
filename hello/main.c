d87c84c223aee3ca7a69695ac59fc22799980cfe
Alec Newman <alecnwmn904@gmail.com>
2015-09-21 19:05:18 -0400

    changed hello

--- hello/main.c
+++ hello/main.c
@@ -30,6 +30,11 @@ int main(void) {
         fake.cells[cols+start+2].codes[0] = 'r';
         fake.cells[cols+start+3].codes[0] = 'l';
         fake.cells[cols+start+4].codes[0] = 'd';
+        fake.cells[cols+start+0].style.back.rgb = 0x000000;
+        fake.cells[cols+start+1].style.back.rgb = 0x101010;
+        fake.cells[cols+start+2].style.back.rgb = 0x202020;
+        fake.cells[cols+start+3].style.back.rgb = 0x303030;
+        fake.cells[cols+start+4].style.back.rgb = 0x404040;
 
         fake.cells[start + (i + 0) % 5].style.fore.rgb = 0xff0000;
         fake.cells[start + (i + 0) % 5].style.back.rgb = 0x00ffff;
